b1a03ca0e0c5358d3da7efae19cd7bb69ed5d872
rizal.rusdiansyah <rizal.rusdiansyah@runsystem.id>
2024-04-18 15:18:31 +0700

    Add alpinejs for validating password input

--- controller/login.go
+++ controller/login.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"bytes"
 	"html/template"
 	"net/http"
 	"path"
@@ -29,32 +30,32 @@ func LoginView() http.HandlerFunc {
 
 func Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// password := r.FormValue("password")
-		// if password != "rizalrizal" {
-		// 	tmpl, err := template.ParseFiles(
-		// 		path.Join("view", "components", "login", "error_message.html"),
-		// 	)
-		// 	if err != nil {
-		// 		w.WriteHeader(http.StatusInternalServerError)
-		// 		w.Write([]byte(err.Error()))
-		// 		return
-		// 	}
+		password := r.FormValue("password")
+		if password != "rizalrizal" {
+			tmpl, err := template.ParseFiles(
+				path.Join("view", "components", "login", "error_message.html"),
+			)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
 
-		// 	b := new(bytes.Buffer)
-		// 	err = tmpl.Execute(b, nil)
-		// 	if err != nil {
-		// 		w.WriteHeader(http.StatusInternalServerError)
-		// 		w.Write([]byte(err.Error()))
-		// 		return
-		// 	}
+			b := new(bytes.Buffer)
+			err = tmpl.Execute(b, nil)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
 
-		// 	// should be 401 Unauthorized but make HTMX error
-		// 	w.WriteHeader(http.StatusOK)
-		// 	w.Write(b.Bytes())
-		// 	return
-		// }
+			// should be 401 Unauthorized but make HTMX error
+			w.WriteHeader(http.StatusOK)
+			w.Write(b.Bytes())
+			return
+		}
 
-		// w.WriteHeader(http.StatusOK)
-		// w.Header().Add("hx-redirect", "/dashboard")
+		w.Header().Add("hx-redirect", "/dashboard")
+		w.WriteHeader(http.StatusFound)
 	}
 }
--- server/routing.go
+++ server/routing.go
@@ -10,6 +10,6 @@ func (s *server) mapRoute() {
 	s.Router.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	s.Router.Get("/dashboard/login", controller.LoginView())
-	s.Router.Post("/dashboard/login", controller.Login())
+	s.Router.Post("/login", controller.Login())
 	s.Router.Get("/dashboard", controller.DashboardView())
 }
